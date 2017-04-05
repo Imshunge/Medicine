16d48e791aed5b3aa52eed02dba99f28925075ea
wbf <852172891@qq.com>
2017-04-05 18:15:04 +0800

    视频第一个界面

---
+++ app/src/main/java/com/shssjk/activity/person/LocalVideoListActivity.java
@@ -0,0 +1,29 @@
+package com.shssjk.activity.person;
+import android.os.Bundle;
+import com.shssjk.activity.BaseActivity;
+import com.shssjk.activity.R;
+
+public class LocalVideoListActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_vide_list);
+        init();
+    }
+
+    @Override
+    public void initSubViews() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initEvent() {
+
+    }
+}
